cf9697d5bf546b881d3ce5f2b5db446235c8483f
Tyler Margison <kolorahl@gmail.com>
2015-10-21 14:18:57 -0700

    Initial code commit

---
+++ lib/presentable.ex
@@ -0,0 +1,44 @@
+defprotocol Presentable do
+  @doc "Returns a 'presentable' data object for client rendering."
+  @fallback_to_any true
+  def present(model)
+end
+
+defimpl Presentable, for: PID do
+  @doc "Assumes a GenServer PID. Presents the process state, if accessible."
+  def present(pid) do
+    Presentable.present(GenServer.call(pid, :state))
+  end
+end
+
+defimpl Presentable, for: List do
+  @doc """
+  Presents each individual element. In the case of a keyword list, presents each
+  value while retaining the original key.
+  """
+  def present(list) do
+    Enum.map(list, &nested/1)
+  end
+
+  defp nested({key, val}) do
+    {key, Presentable.present(val)}
+  end
+
+  defp nested(val), do: Presentable.present(val)
+end
+
+defimpl Presentable, for: Map do
+  @doc "Present each element of the map, retaining the original key."
+  def present(map) do
+    Enum.into(map, %{}, fn({k, v}) ->
+      {k, Presentable.present(v)}
+    end)
+  end
+end
+
+defimpl Presentable, for: Any do
+  @doc """
+  Returns the data as-is.
+  """
+  def present(data), do: data
+end
---
+++ lib/presenter.ex
@@ -0,0 +1,46 @@
+defmodule Presenter do
+  @moduledoc """
+  Include this module via `use Presenter` to add an auto-generated `Presentable`
+  protocol implementation for any module that defines a struct.
+  """
+
+  @type attribute
+
+  @doc false
+  defmacro __using__(_) do
+    quote do
+      import Presenter
+    end
+  end
+
+  @spec attributes([atom | {atom, function}]) :: Macro.t
+  defmacro attributes(attributes \\ []) do
+    quote do
+      defimpl Presentable, for: __MODULE__ do
+        def present(model) do
+          attributes = prepare(model, unquote(attributes))
+          Enum.into(attributes, %{}, fn
+            ({attribute, transform}) ->
+              present_attribute(model, attribute, transform)
+            (attribute) ->
+              present_attribute(model, attribute, &Presentable.present/1)
+          end)
+        end
+
+        @spec prepare(Map.t, [atom]) :: Map.t
+        defp prepare(model, []), do: Map.keys(model) |> List.delete(:__struct__)
+        defp prepare(model, attributes), do: Enum.uniq_by(attributes, &attr_name/1)
+
+        @spec attr_name(atom | {atom, function}) :: atom
+        defp attr_name({name, _}), do: name
+        defp attr_name(name), do: name
+
+        @spec present_attribute(Map.t, atom, function) :: {atom, term}
+        defp present_attribute(model, attribute, transform) do
+          value = Map.get(model, attribute)
+          {attribute, transform.(value)}
+        end
+      end
+    end
+  end
+end
