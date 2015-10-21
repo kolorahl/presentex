bf3273db1ab38e678891c86b9890b951dba9ea66
Tyler Margison <kolorahl@gmail.com>
2015-10-21 14:31:43 -0700

    Add tests

--- test/presentex_test.exs
+++ test/presentex_test.exs
@@ -1,8 +1,33 @@
 defmodule PresentexTest do
   use ExUnit.Case
-  doctest Presentex
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  defmodule ImplicitAttrs do
+    use Presenter
+    defstruct [one: "swamp", two: 10, three: "thing"]
+    attributes
+  end
+
+  defmodule ExplicitAtomOnly do
+    use Presenter
+    defstruct [one: "swamp", two: 10, three: "thing"]
+    attributes [:one, :two]
+  end
+
+  defmodule ExplicitAttrs do
+    use Presenter
+    defstruct [one: "swamp", two: 10, three: "thing"]
+    attributes [{:one, fn(x) -> String.length(x) end}, {:two, fn(x) -> x * x end}, :three]
+  end
+
+  test "works with implicit attribute list" do
+    %{one: "swamp", two: 10, three: "thing"} = Presentable.present(%ImplicitAttrs{})
+  end
+
+  test "works with explicit attribute (atom-only) list" do
+    %{one: "swamp", two: 10} = Presentable.present(%ExplicitAtomOnly{})
+  end
+
+  test "works with explicit attribute (mixed-type) list" do
+    %{one: 5, two: 100, three: "thing"} = Presentable.present(%ExplicitAttrs{})
   end
 end
